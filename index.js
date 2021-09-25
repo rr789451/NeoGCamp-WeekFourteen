d4406742af71d8e92bdde48c6dabe26792a401c9
Rohit <rr789451@gmail.com>
2021-09-26 05:26:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const initialPrice = document.querySelector('.initial-price');
+const quantity = document.querySelector('.quantity');
+const currentPrice = document.querySelector('.current-price');
+const submit = document.querySelector('#btn');
+const output = document.querySelector('#output');
+
+function calculateProfitAndLoss(initial, current){
+    if(current > initial){
+        let profit = (current - initial);
+        let profitPercentage = (profit/initial)*100;
+        output.style.color = "green";
+        output.innerText = "Hey, the profit is "+profit+" and the percent is "+profitPercentage+"%";
+    }
+    else if(initial > current){
+        let loss = (initial - current);
+        let lossPercentage = (loss/initial)*100;
+        output.style.color = "red"
+        output.innerText = "Hey, the loss is "+loss+" and the percent is "+lossPercentage+"%";
+    }
+}
+
+function submitHandler(){
+    const initial = initialPrice.value * quantity.value;
+    const current = currentPrice.value * quantity.value;
+    if(initial > 0 && current > 0 && quantity.value > 0){
+        calculateProfitAndLoss(initial, current);
+    }
+    else{
+        alert("Please fill all the fields");
+    }
+}
+
+submit.addEventListener("click", submitHandler);
